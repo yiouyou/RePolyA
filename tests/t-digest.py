2da31cc7eb1f1dd328ed0b3ce50ab7add5b33617
yiouyou <zhuosong@gmail.com>
2023-09-09 05:22:29 +0000

    0909

--- tests/t-digest.py
+++ tests/t-digest.py
@@ -9,6 +9,6 @@ from repolya.paper._digest.pdf import get_imgs_from_pdf, get_text_from_pdf
 
 _fp = PAPER_PDF / 'Abanades2021ABlooperFA_10.1101_2021.07.26.453747.pdf'
 
-# get_imgs_from_pdf(_fp)
+get_imgs_from_pdf(_fp)
 get_text_from_pdf(_fp)
 
