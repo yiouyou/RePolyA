69ec0e2a3397dbe51a990fdb114444fb19921c11
yiouyou <zhuosong@gmail.com>
2023-10-10 04:50:22 +0000

    1010

--- tests/autogen/test.py
+++ tests/autogen/test.py
@@ -7,39 +7,43 @@ from repolya._const import AUTOGEN_REF
 from repolya.autogen.workflow import do_simple_task, do_simple_code, do_rd, do_math, do_plan_task, do_res, do_rag_doc, do_rag_code
 
 
-_do = int(sys.argv[1])
+_n = int(sys.argv[1])
 
-if _do == 1:
+if _n == 1:
     re = do_simple_task("查找一下长沙的天气.")
     print(f"{re}")
 
 
-if _do == 2:
+if _n == 2:
     re = do_simple_code("For certain cloud services (such as computing), assuming there are several given attributes (such as vCore, memory, iops, storage, backup, etc.) of each computing resource sku, write a flexible and basic recommendation class to filter out unfitted skus and find the lowest price as the recommendation for customers.")
     print(f"{re}")
 
 
-if _do == 3:
-    re = do_rd("UDP-GlcA大规模生成")
+if _n == 3:
+    # _do = "UDP-GlcA大规模生成"
+    _do = "查找UDP-Glc，UDP-GlcA，orotate，hypotaurine各自的量产菌株，以及大规模生成的方法。以列表的形式输出结果，并给出参考文献。"
+    re = do_rd(_do)
     print(f"{re}")
 
 
-if _do == 4:
+if _n == 4:
     re = do_math("Find all numbers $a$ for which the graph of $y=x^2+a$ and the graph of $y=ax$ intersect. Express your answer in interval notation.")
     print(f"{re}")
 
 
-if _do == 5:
+if _n == 5:
     re = do_plan_task("Suggest a fix to an open good first issue of flaml")
     print(f"{re}")
 
 
-if _do == 6:
-    re = do_res("find papers on LLM applications from arxiv in the last week, create a markdown table of different domains.")
+if _n == 6:
+    # _do = "find papers on LLM applications from arxiv in the last week, create a markdown table of different domains."
+    _do = "查找UDP-Glc，UDP-GlcA，orotate，hypotaurine各自的量产菌株，以及大规模生成的方法。以列表的形式输出结果，并给出参考文献。"
+    re = do_res(_do)
     print(f"{re}")
 
 
-if _do == 7:
+if _n == 7:
     re = do_rag_doc(
         msg="Which film came out first, Blind Shaft or The Mask Of Fu Manchu?",
         search_string="",
@@ -49,7 +53,7 @@ if _do == 7:
     print(f"{re}")
 
 
-if _do == 8:
+if _n == 8:
     re = do_rag_code(
         msg="How can I use FLAML to perform a classification task and use spark to do parallel training. Train 30 seconds and force cancel jobs if time limit is reached.",
         search_string="spark",
@@ -59,7 +63,7 @@ if _do == 8:
     print(f"{re}")
 
 
-if _do == 77:
+if _n == 77:
     from autogen.retrieve_utils import create_vector_db_from_dir, query_vector_db
     import chromadb
     print("Trying to create collection.")
@@ -68,24 +72,27 @@ if _do == 77:
         max_tokens=16000*0.4,
         client=chromadb.Client(),
         collection_name="rag_doc",
-        embedding_model="all-MiniLM-L12-v2",
+        embedding_model="all-mpnet-base-v2", #"all-MiniLM-L12-v2",
         chunk_mode="one_line",
         must_break_at_empty_line=True,
         get_or_create=False,
     )
-    problem = "who is Maheen Khan?"
+    problem = "Which film came out first, Blind Shaft or The Mask Of Fu Manchu?" #"who is Maheen Khan?"
     results = query_vector_db(
         query_texts=[problem],
         n_results=10,
         search_string="",
         client=chromadb.Client(),
         collection_name="rag_doc",
-        embedding_model="all-MiniLM-L12-v2",
+        embedding_model="all-mpnet-base-v2", #"all-MiniLM-L12-v2",
     )
     print("doc_ids: ", results["ids"])
+    for i in results["ids"][0]:
+        i_txt = results["ids"][0][i]
+        print(f"{i}: '{i_txt}'")
 
 
-if _do == 88:
+if _n == 88:
     from autogen.retrieve_utils import create_vector_db_from_dir, query_vector_db
     import chromadb
     print("Trying to create collection.")
@@ -109,4 +116,7 @@ if _do == 88:
         embedding_model="all-mpnet-base-v2",
     )
     print("doc_ids: ", results["ids"])
+    for i in results["ids"][0]:
+        i_txt = results["ids"][0][i]
+        print(f"{i}: '{i_txt}'")
 
